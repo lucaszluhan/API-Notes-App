3c9daacb8647fd2e48f89291016fdf359608a79c
Lucas Zluhan <lszluhan@hotmail.com>
2023-09-08 12:28:52 -0300

    set server message at default route

--- src/core/presentation/server/routes.ts
+++ src/core/presentation/server/routes.ts
@@ -1,8 +1,21 @@
-import express from 'express';
-import NotesRoutes from '../../../features/notes/presentation/routes/notesRoutes';
-import UsersRoutes from '../../../features/users/presentation/routes/usersRoutes';
+import express, { Router, Request, Response } from 'express'
+import NotesRoutes from '../../../features/notes/presentation/routes/notesRoutes'
+import UsersRoutes from '../../../features/users/presentation/routes/usersRoutes'
+import { ok, serverError } from '../helpers/httpHandlers'
 
 export const makeRoutes = (app: express.Application) => {
-   app.use('/users', UsersRoutes.initRoutes());
-   app.use('/notes', NotesRoutes.initRouter());
-};
+  const router = Router()
+
+  app.use('/users', UsersRoutes.initRoutes())
+  app.use('/notes', NotesRoutes.initRouter())
+  app.use(
+    '/',
+    router.get('/', (_, res: Response) => {
+      try {
+        return ok(res, 'ok')
+      } catch (error) {
+        return serverError(res, 'Falha!', error)
+      }
+    })
+  )
+}
